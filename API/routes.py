7c10cde53ef19572a94120a321b3526bc727e43a
Matheus Pereira <matheusgp.mto@outlook.com>
2023-05-18 00:51:54 -0300

    Finalizando a criação dos templates HTML

--- API/routes.py
+++ API/routes.py
@@ -4,3 +4,7 @@ from API import app, render_template, url_for
 @app.route("/")
 def home():
     return render_template("login.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
